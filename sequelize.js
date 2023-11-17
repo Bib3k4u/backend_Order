9574a0ce53f3ba6cb040afbc74e6167a705ed176
Bibek <zhabibek4u@gmail.com>
2023-11-17 09:50:09 +0530

    f

--- sequelize.js
+++ sequelize.js
@@ -1,3 +1,20 @@
+// // sequelize.js
+// const { Sequelize } = require('sequelize');
+// const config = require('./config/config.json')[process.env.NODE_ENV || 'development'];
+
+// const sequelize = new Sequelize(
+//   config.database,
+//   config.username,
+//   config.password,
+//   {
+//     host: config.host,
+//     dialect: config.dialect,
+//     operatorsAliases: config.operatorsAliases,
+//   }
+// );
+
+// module.exports = sequelize;
+
 // sequelize.js
 const { Sequelize } = require('sequelize');
 const config = require('./config/config.json')[process.env.NODE_ENV || 'development'];
@@ -9,8 +26,9 @@ const sequelize = new Sequelize(
   {
     host: config.host,
     dialect: config.dialect,
-    operatorsAliases: config.operatorsAliases,
+    operators: false, // Use this instead of operatorsAliases
   }
 );
 
 module.exports = sequelize;
+
