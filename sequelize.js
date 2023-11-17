ae84e75f7792e269fc913bf2f246308b1a9327ed
Bibek <zhabibek4u@gmail.com>
2023-11-17 10:09:31 +0530

    f

--- sequelize.js
+++ sequelize.js
@@ -1,21 +1,4 @@
-// // sequelize.js
-// const { Sequelize } = require('sequelize');
-// const config = require('./config/config.json')[process.env.NODE_ENV || 'development'];
 
-// const sequelize = new Sequelize(
-//   config.database,
-//   config.username,
-//   config.password,
-//   {
-//     host: config.host,
-//     dialect: config.dialect,
-//     operatorsAliases: config.operatorsAliases,
-//   }
-// );
-
-// module.exports = sequelize;
-
-// sequelize.js
 const { Sequelize } = require('sequelize');
 const config = require('./config/config.json')[process.env.NODE_ENV || 'development'];
 
@@ -26,10 +9,17 @@ const sequelize = new Sequelize(
   {
     host: config.host,
     dialect: config.dialect,
-    port: 3306,
     operators: false, // Use this instead of operatorsAliases
   }
 );
 
 module.exports = sequelize;
 
+// module.exports = {
+//   development: {
+//     username: uihnjgtnvoqbtrzn,
+//     password: B8Raxzzj5mNDlnTkZJ8i,
+//     database: bjjyouxhtjqgyszcu4wq,
+//     host: 'bjjyouxhtjqgyszcu4wq-mysql.services.clever-cloud.com',
+//     dialect: mysql,
+//   },
